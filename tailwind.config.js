ec76d5b04a9721638671c8f417645ae8ef79951f
Mizuki <mizuki@Yota-KnoMacBook-Pro.local>
2026-01-08 19:53:53 +0900

    feat/add-tailwind-css

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {
+      colors: {
+        daidai: "#ee7800",
+        azuki: "#96514d",
+      },
+    },
+  },
+  plugins: [],
+};
